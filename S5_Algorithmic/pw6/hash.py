4b3be551a1f6d4d2ac01bcf4e409d4da5c3728e5
Inkkonu <killian.blain@outlook.com>
2023-04-23 22:57:02 +0200

    Quick refactor of pw6

--- S5_Algorithmic/pw6/hash.py
+++ S5_Algorithmic/pw6/hash.py
@@ -1,14 +1,14 @@
 from random import randint
 
 
-def hachage(s: str, m: int):
+def hash(s: str, m: int):
     return compress(encode(s), m)
 
 
 def encode(s: str) -> int:
     total = 0
     for i in range(len(s)):
-        total += ord(s[i]) * 97 ** i
+        total += ord(s[i]) * 97**i
     return total
 
 
@@ -16,6 +16,5 @@ def compress(n: int, m: int) -> int:
     return (30 * n + 85) % m
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print("h")
-
--- S5_Algorithmic/pw6/item.py
+++ S5_Algorithmic/pw6/item.py
@@ -1,8 +1,7 @@
 class Item:
-
     def __init__(self, key: str, value: int):
         self.key = key
         self.value = value
 
     def __str__(self):
-        return f'Key = {self.key}, value = {self.value}'
+        return f"Key = {self.key}, value = {self.value}"
